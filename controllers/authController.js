e95a43a0e062005636288915b3bf43332676b8ee
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-08-17 21:35:39 -0300

    server set up with register controller, errors, middlewares, routes and user model

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError } from '../errors/index.js'
+
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        throw new BadRequestError('please provide all values')
+    }
+
+    const userAlreadyExists = await User.findOne({ email })
+
+    if (userAlreadyExists) {
+        throw new BadRequestError('Email already exists')
+    }
+
+    const user = await User.create({ name, email, password }) 
+    
+    res.status(StatusCodes.OK).json({ user })
+}
+
+const login = async (req, res) => {
+    res.send('login user')
+}
+
+const updateUser = async (req, res) => {
+    res.send('updateUser')
+}
+
+export { register, login, updateUser }
---
+++ server.js
@@ -0,0 +1,43 @@
+import 'express-async-errors'
+import express from 'express'
+
+import connectDB from './db/connect.js'
+
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
+
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome!')
+})
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
