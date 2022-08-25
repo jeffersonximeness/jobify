0dbe8fffa774882897fa95ee3641d1d9f90c1eb2
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-08-24 21:47:11 -0300

    morgan http middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import 'express-async-errors'
+import morgan from 'morgan'
 import cors from 'cors'
 import express from 'express'
 
@@ -16,6 +17,11 @@ dotenv.config()
 const app = express()
 
 app.use(cors())
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
 app.get('/api/v1', (req, res) => {
