0eb0ee237f19df285c8b52e95001a959600fe8d9
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-08-24 21:48:58 -0300

    login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -18,7 +18,7 @@ const register = async (req, res) => {
     const user = await User.create({ name, email, password })
     const token = user.createJWT()
     
-    res.status(StatusCodes.OK)
+    res.status(StatusCodes.CREATED)
         .json({ 
             user: {
                 name: user.name,
@@ -32,7 +32,33 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide all values')
+    }
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        throw new UnAuthenticatedError('Invalid credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+
+    if (!isPasswordCorrect) {
+        throw new UnAuthenticatedError('Invalid credentials')
+    }
+
+    const token = user.createJWT()
+    user.password = undefined
+
+    res.status(StatusCodes.OK)
+        .json({
+            user,
+            token,
+            location: user.location
+        })
 }
 
 const updateUser = async (req, res) => {
