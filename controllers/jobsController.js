a7c64407aacf616a22698307e08d0bc99f67b5fe
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-09-06 19:18:30 -0300

    edit job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,6 @@
 import Job from '../models/Job.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
+import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js'
 
 
 const createJob = async (req, res) => {
@@ -27,12 +27,30 @@ const getAllJobs = async (req, res) => {
         })
 }
 
-const deleteJob = async (req, res) => {
-    res.send('delete job')
+const updateJob = async (req, res) => {
+    const { id: jobId } = req.params
+    const { company, position } = req.body
+
+    if (!company || !position) {
+        throw new BadRequestError('Please provide all values')
+    }
+
+    const job = await Job.findOne({ _id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id: ${jobId}`)
+    }
+
+    const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(StatusCodes.OK).json({ updatedJob })
 }
 
-const updateJob = async (req, res) => {
-    res.send('update job')
+const deleteJob = async (req, res) => {
+    res.send('delete job')
 }
 
 const showStats = async (req, res) => {
