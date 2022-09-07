05a391d619c2f301c304f298890713564fde0ed8
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-09-07 18:57:02 -0300

    aggregation based on month and year

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes'
 import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js'
 import checkPermissions from '../utils/checkPermission.js'
 import mongoose from 'mongoose'
+import moment from 'moment'
 
 const createJob = async (req, res) => {
     const { company, position } = req.body
@@ -87,7 +88,29 @@ const showStats = async (req, res) => {
         declined: stats.declined || 0,
         interview: stats.interview || 0,
     }
-    let monthlyApplications = []
+
+    let monthlyApplications = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+        {
+            $group: {
+                _id: {
+                    year: { $year: '$createdAt' },
+                    month: { $month: '$createdAt' }
+                },
+                count: { $sum: 1 }
+            }
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 }
+    ])
+
+    monthlyApplications = monthlyApplications.map((item) => {
+        const { _id: { year, month }, count } = item
+
+        const date = moment().month(month - 1).year(year).format('MMM Y')
+        
+        return { date, count }
+    }).reverse()
 
     res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
