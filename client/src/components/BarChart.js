31ae841029bfb236a164624f48fac138671c4bd7
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-09-07 19:23:57 -0300

    Bar Chart

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,10 +1,29 @@
-function BarChart() {
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from 'recharts'
+
+function BarChartComponent({ data }) {
 
     return (
-        <div>
-            <h2>Bar chart</h2>
-        </div>
+        <ResponsiveContainer width="100%" height={300}>
+            <BarChart
+                data={data}
+                margin={{ top: 50 }}
+            >
+                <CartesianGrid strokeDasharray='3 3'/>
+                <XAxis dataKey='date'/>
+                <YAxis allowDecimals={false}/>
+                <Tooltip />
+                <Bar dataKey='count' fill='#2cb1bc' barSize={75}/>
+            </BarChart>
+        </ResponsiveContainer>
     )
 }
 
-export default BarChart
+export default BarChartComponent
